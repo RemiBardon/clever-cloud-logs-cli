775adc420a101e734505ab3588c50b4260a3d236
Rémi Bardon <remi.bardon.dev@gmail.com>
2020-10-08 22:55:49 +0200

    Add argument parsing logic

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+extern crate clap;
+use clap::{Arg, App};
+
 fn main() {
-    println!("Hello, world!");
+    let matches = App::new("Clever Cloud App Logs CLI")
+        .version("1.0")
+        .author("Rémi B. <remi.bardon.dev@gmail.com>")
+        .about("Streams app logs from Clever Cloud API")
+        .arg(Arg::with_name("APP_ID")
+            .long("id")
+            .help("Sets the app ID to use")
+            .required(true)
+            .index(1)
+        )
+        .get_matches();
+
+    // Calling .unwrap() is safe here because "APP_ID" is required
+    println!("Using app ID: {}", matches.value_of("APP_ID").unwrap());
+
+    // More program logic goes here...
 }
