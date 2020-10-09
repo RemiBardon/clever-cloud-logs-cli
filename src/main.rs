1b67d70ef99bf7ced69a8a41047e754663a90c09
Rémi Bardon <remi.bardon.dev@gmail.com>
2020-10-09 15:13:07 +0200

    [WIP] Add Clever Cloud CLI config file decoding

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,8 @@ use directories::UserDirs;
 use std::io::prelude::*;
 use std::fs::File;
 
+use serde_json::Value;
+
 fn main() {
     let matches = App::new("Clever Cloud App Logs CLI")
         .version("1.0")
@@ -25,14 +27,8 @@ fn main() {
             .required(true)
         )
         .get_matches();
-        
-    let user_dirs = UserDirs::new().expect("Could not find user home directory");
-    let config_file_path = format!("{}/.config/clever-cloud", user_dirs.home_dir().display());
 
-    let mut config_file = File::open(config_file_path).expect("No file found at '~/.config/clever-cloud'. Please follow instructions at https://github.com/CleverCloud/clever-tools to install 'clever-tools', then run `clever login`.");
-    
-    let mut config = String::new();
-    config_file.read_to_string(&mut config).expect("Unable to read the file at '~/.config/clever-cloud'");
+    let (access_token, token_secret) = clever_config();
 
     // Calling .unwrap() is safe here because "APP_ID" is required
     let app_id = matches.value_of("APP_ID").unwrap();
@@ -40,14 +36,53 @@ fn main() {
     // Create HTTP endpoint
     let mut endpoint = format!("https://api.clever-cloud.com:443/v2/logs/{}/sse", app_id);
 
-    // FIXME: Replace config by actual access_token value
-    let access_token = config;
-
     // Add HTTP query parameters
     // FIXME: Find how is built o_auth_authorization_string
-    let o_auth_authorization_string = access_token;
+    let o_auth_authorization_string: &str = access_token.as_str();
     endpoint = format!("{}?authorization={}", endpoint, o_auth_authorization_string);
     println!("endpoint: {}", endpoint);
 
     // More program logic goes here...
+}
+
+fn parsed_config() -> serde_json::Value {
+    let user_dirs = UserDirs::new().expect("Could not find user home directory");
+    let config_file_path = format!("{}/.config/clever-cloud", user_dirs.home_dir().display());
+
+    let mut config_file = File::open(config_file_path)
+        .expect("No file found at '~/.config/clever-cloud'. Please follow instructions at https://github.com/CleverCloud/clever-tools to install 'clever-tools', then run `clever login`.");
+    
+    let mut json_config = String::new();
+    config_file.read_to_string(&mut json_config)
+        .expect("Unable to read the file at '~/.config/clever-cloud'");
+
+    // Parse the string of data into serde_json::Value
+    serde_json::from_str(json_config.as_str())
+        .expect("Invalid content in file '~/.config/clever-cloud'")
+}
+
+fn clever_config() -> (String, String) {
+    let config = parsed_config();
+
+    // Get OAuth1 Access Token
+    let access_token: String;
+    let token_key = "token";
+    if let Value::String(token) = config[token_key].clone() {
+        access_token = token;
+    } else {
+        println!("No value '{}' found in '~/.config/clever-cloud'. Please follow instructions at https://github.com/CleverCloud/clever-tools to install 'clever-tools', then run `clever login`.", token_key);
+        std::process::exit(1);
+    }
+    
+    // Get OAuth1 API Secret
+    let token_secret: String;
+    let secret_key = "secret";
+    if let Value::String(secret) = config[secret_key].clone() {
+        token_secret = secret;
+    } else {
+        println!("No value '{}' found in '~/.config/clever-cloud'. Please follow instructions at https://github.com/CleverCloud/clever-tools to install 'clever-tools', then run `clever login`.", secret_key);
+        std::process::exit(1);
+    }
+
+    (access_token, token_secret)
 }
