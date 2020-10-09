6d56cc43b93e55bcff595cde3ff886af65f06764
Rémi Bardon <remi.bardon.dev@gmail.com>
2020-10-09 15:38:43 +0200

    Add OAuth 1 authorization string query parameter to SSE endpoint

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,21 @@
+// Command Line Argument Parsing
 extern crate clap;
 use clap::{Arg, App};
 
+// File reading & JSON parsing
 use directories::UserDirs;
 use std::io::prelude::*;
 use std::fs::File;
-
 use serde_json::Value;
+
+// OAuth 1
+extern crate rand;
+use std::time::{SystemTime, UNIX_EPOCH};
+use rand::Rng; 
+use rand::distributions::Alphanumeric;
+
+#[derive(oauth::Authorize)]
+struct EmptyRequest {}
 
 fn main() {
     let matches = App::new("Clever Cloud App Logs CLI")
@@ -18,11 +28,19 @@ fn main() {
             .required(true)
             .index(1)
         )
-        .arg(Arg::with_name("token")
-            .short("t")
-            .long("token")
-            .value_name("ACCESS_TOKEN")
-            .help("OAuth1 access token used for Clever Cloud API authentication. For more information, visit https://www.clever-cloud.com/doc/clever-cloud-apis/cc-api/#http-calls.")
+        .arg(Arg::with_name("consumer_key")
+            .short("k")
+            .long("consumer_key")
+            .value_name("OAUTH_CONSUMER_KEY")
+            .help("OAuth1 consumer key used for Clever Cloud API authentication. For more information, visit https://www.clever-cloud.com/doc/clever-cloud-apis/cc-api/#-create-consumers-tokens-.")
+            .takes_value(true)
+            .required(true)
+        )
+        .arg(Arg::with_name("consumer_secret")
+            .short("s")
+            .long("consumer_secret")
+            .value_name("OAUTH_CONSUMER_SECRET")
+            .help("OAuth1 consumer secret used for Clever Cloud API authentication. For more information, visit https://www.clever-cloud.com/doc/clever-cloud-apis/cc-api/#-create-consumers-tokens-.")
             .takes_value(true)
             .required(true)
         )
@@ -30,16 +48,34 @@ fn main() {
 
     let (access_token, token_secret) = clever_config();
 
-    // Calling .unwrap() is safe here because "APP_ID" is required
-    let app_id = matches.value_of("APP_ID").unwrap();
+    // Calling .expect() is safe here because "APP_ID" is required
+    let app_id = matches.value_of("APP_ID").expect("Argument 'APP_ID' not found");
 
     // Create HTTP endpoint
     let mut endpoint = format!("https://api.clever-cloud.com:443/v2/logs/{}/sse", app_id);
 
-    // Add HTTP query parameters
-    // FIXME: Find how is built o_auth_authorization_string
-    let o_auth_authorization_string: &str = access_token.as_str();
-    endpoint = format!("{}?authorization={}", endpoint, o_auth_authorization_string);
+    // Calling .expect() is safe here because "consumer_key" is required
+    let consumer_key = matches.value_of("consumer_key").expect("Argument 'consumer_key' not found");
+    // Calling .expect() is safe here because "consumer_secret" is required
+    let consumer_secret = matches.value_of("consumer_secret").expect("Argument 'consumer_secret' not found");
+
+    let client = oauth::Credentials::new(consumer_key, consumer_secret);
+    let token = oauth::Credentials::new(access_token.as_str(), token_secret.as_str());
+
+    let mut builder = oauth::Builder::new(client, oauth::HmacSha1);
+    let nonce = rand_string(16);
+    builder
+        .token(token)
+        .nonce(nonce.as_str())
+        .timestamp(timestamp());
+
+    let oauth::Request {
+        authorization,
+        data: _,
+    } = builder.get(endpoint.as_str(), EmptyRequest{});
+
+    // Add OAuth 1 authorization string query parameter
+    endpoint = format!("{}?authorization={}", endpoint, authorization);
     println!("endpoint: {}", endpoint);
 
     // More program logic goes here...
@@ -85,4 +121,18 @@ fn clever_config() -> (String, String) {
     }
 
     (access_token, token_secret)
+}
+
+fn timestamp() -> u64 {
+    match SystemTime::now().duration_since(UNIX_EPOCH) {
+        Ok(n) => n.as_secs(),
+        Err(_) => panic!("SystemTime before UNIX EPOCH!"),
+    }
+}
+
+fn rand_string(count: usize) -> String {
+    rand::thread_rng()
+        .sample_iter(&Alphanumeric)
+        .take(count)
+        .collect::<String>()
 }
