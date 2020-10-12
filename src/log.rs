411d6a152a4807dddb86dd9d07e2a3bb862d248f
Rémi Bardon <remi.bardon.dev@gmail.com>
2020-10-12 18:16:00 +0200

    Add terminal formatting (colored logs)

--- src/log.rs
+++ src/log.rs
@@ -1,4 +1,8 @@
+// JSON Parsing
 use serde::{Deserialize};
+
+// Terminal Formatting
+extern crate term;
 
 #[derive(Deserialize)]
 struct CCEventMessage {
@@ -8,27 +12,66 @@ struct CCEventMessage {
 
 #[derive(Deserialize)]
 struct CCEventSource {
+    syslog_program: String,
+
     #[serde(rename = "@timestamp")]
     timestamp: String,
     #[serde(rename = "@message")]
     message: String,
 }
 
-fn format_log_line(line: &str) -> Option<String> {
-    let deserialized: CCEventMessage = {
-        match serde_json::from_str(line) {
-            Ok(d) => d,
-            Err(_) => return None,
-        }
-    };
-    let source = &deserialized.source;
-    let (timestamp, message) = (source.timestamp.as_str(), source.message.as_str());
+fn is_clever_message(source: &CCEventSource) -> bool {
+    return source.syslog_program == "/home/bas/rubydeployer/deployer.rb";
+}
 
-    Some(format!("{}: {}", timestamp, message))
+fn is_deployment_success_message(source: &CCEventSource) -> bool {
+    return is_clever_message(source)
+        && source.message
+            .to_lowercase().as_str()
+            .starts_with("successfully deployed in");
+}
+
+fn is_deployment_failed_message(source: &CCEventSource) -> bool {
+    return is_clever_message(source)
+        && source.message
+            .to_lowercase().as_str()
+            .starts_with("deploy failed in");
+}
+
+fn is_build_sucess_message(source: &CCEventSource) -> bool {
+    return source.message
+        .to_lowercase().as_str()
+        .starts_with("build succeeded in");
 }
 
 pub fn log(line: &str) {
-    if let Some(formatted) = format_log_line(line) {
-        println!("{}", formatted);
+    let deserialized: CCEventMessage = {
+        match serde_json::from_str(line) {
+            Ok(d) => d,
+            Err(_) => return,
+        }
+    };
+
+    let source = &deserialized.source;
+
+    print!("{}: ", source.timestamp.as_str());
+
+    let message = source.message.as_str();
+    let mut terminal = term::stdout().unwrap();
+    if is_deployment_success_message(source) {
+        terminal.fg(term::color::GREEN).unwrap();
+        terminal.attr(term::Attr::Bold).unwrap();
+        println!("{}", message);
+    } else if is_deployment_failed_message(source) {
+        terminal.fg(term::color::RED).unwrap();
+        terminal.attr(term::Attr::Bold).unwrap();
+        println!("{}", message);
+    } else if is_build_sucess_message(source) {
+        terminal.fg(term::color::BLUE).unwrap();
+        terminal.attr(term::Attr::Bold).unwrap();
+        println!("{}", message);
+    } else {
+        println!("{}", message);
     }
+    terminal.reset().unwrap();
 }
